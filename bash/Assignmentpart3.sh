95125ba78ab5527e8495c2063e73e20b7585def7
EdwinChan98 <edwinchan981018@yahoo.com.hk>
2018-11-06 15:47:12 -0500

    Ass3

--- bash/Assignmentpart3.sh
+++ bash/Assignmentpart3.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 dpkg -S filepath
 echo "What is the command name?"
+enter=0
+while [[ $enter="" ]]; do
+  case $1 in
+    -S
+   exit
+   ;;
+  esac
+echo "ERROR"
+echo "Command didn't come from a software package"
+echo "Command doesn't come from a file"
+done
